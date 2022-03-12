6fc14e295c1dc48bdc232be16d8e1de39964cdd3
sohseyedi <sohseyedi@gmail.com>
2022-03-12 13:40:13 +0330

    end desigen(no responsive)

--- src/Components/layout/Search.jsx
+++ src/Components/layout/Search.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { FiSearch , FiArrowLeft } from 'react-icons/fi';
+import { FiSearch, FiArrowLeft } from 'react-icons/fi';
 import '../Scss/Search.scss'
 
 const Search = () => {
     return (
-        <section className='search' dir='rtl'>
+
+        <section className="search" dir='rtl'>
             <div className="container">
-                <div className=' search-box'>
-                    <input className="search-box__input" placeholder='....' />
-                    <select name="" id="" className="search-box__radio">
-                        <option value="آقا">آقا</option>
-                        <option value="خانم">خانم</option>
-                    </select>
-                    <button className='search-box__btn'><FiSearch size={25}/></button>
+                <div className="search-content">
+                    <div className="search-content__box">
+                        <input className="search-content__box-city" placeholder='شهر مورد نظر' />
+                        <input className="search-content__box-service" placeholder='سرویس مورد نظر' />
+                        <button className='search-content__box-btn'><FiSearch size={32} /></button>
+                        <div className='search-content__box-pip'></div>
+                        <a href="/" className="search-content__box-full">جستجوی بهتر <FiArrowLeft /></a>
+                    </div>
+                    <div className="search-content__support">
+                        <a href="/" className="">درخواست پشتیبانی</a>
+                    </div>
                 </div>
-                <div className="search-full">جستجوی کامل <FiArrowLeft/></div>
             </div>
         </section>
     )
